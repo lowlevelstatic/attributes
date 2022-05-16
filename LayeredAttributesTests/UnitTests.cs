b11d224aaafa2ec7ca4d3e0ecabc8c73393f9ca9
Jim Martin <jim.martin@unity3d.com>
2022-05-16 11:06:50 -0500

    Add test for SetBaseAttribute and GetCurrentAttribute
    Not worth try to test Get and Set independently.
    Skipping the intermediate step of returning a constant for the initial Get implementation.

--- LayeredAttributesTests/UnitTests.cs
+++ LayeredAttributesTests/UnitTests.cs
@@ -21,5 +21,20 @@ namespace LayeredAttributesTests
             // Assert
             Assert.That(() => attributes.GetCurrentAttribute(AttributeKey.Power), Throws.ArgumentException);
         }
+        
+        [Test]
+        public void GetCurrentAttribute_WithBaseAttribute_ReturnsBaseAttribute()
+        {
+            // Arrange
+            var attributes = new SampleLayeredAttributes();
+
+            // Act
+            attributes.SetBaseAttribute(AttributeKey.Power, 5);
+            attributes.SetBaseAttribute(AttributeKey.Toughness, 4);
+            
+            // Assert
+            Assert.AreEqual(5, attributes.GetCurrentAttribute(AttributeKey.Power));
+            Assert.AreEqual(4, attributes.GetCurrentAttribute(AttributeKey.Toughness));
+        }
     }
 }
