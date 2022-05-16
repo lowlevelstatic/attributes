63880afa51a69f17cdefaa2e7d5f89e8ee262ee7
Jim Martin <jim.martin@unity3d.com>
2022-05-15 20:09:40 -0500

    Add failing test for GetCurrentAttribute

---
+++ LayeredAttributesTests/UnitTests.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using LayeredAttributes;
+
+namespace LayeredAttributesTests
+{
+    public class Tests
+    {
+        private const AttributeKey DefaultAttributeKey = AttributeKey.Power;
+        private const int DefaultAttributeValue = 1;
+        
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetCurrentAttribute_byDefault_returnsBaseAttribute()
+        {
+            // Arrange
+            var attributes = new SampleLayeredAttributes(DefaultAttributeKey, DefaultAttributeValue);
+            
+            // Assert
+            Assert.AreEqual(DefaultAttributeValue, attributes.GetCurrentAttribute(DefaultAttributeKey));
+        }
+    }
+}
