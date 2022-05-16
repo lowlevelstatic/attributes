da4e21f4e8fe43531f0187f12522298f712a20fa
Jim Martin <jim.martin@unity3d.com>
2022-05-16 11:43:52 -0500

    Pass AddLayeredEffect_WithAdds_ReturnsTotal

--- LayeredAttributes/SampleLayeredAttributes.cs
+++ LayeredAttributes/SampleLayeredAttributes.cs
@@ -1,22 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LayeredAttributes
 {
     public class SampleLayeredAttributes : ILayeredAttributes
     {
         private readonly Dictionary<AttributeKey, int> m_baseAttributes = new();
+        private readonly List<LayeredEffectDefinition> m_effectDefinitions = new();
 
         public void SetBaseAttribute(AttributeKey attKey, int value) => m_baseAttributes[attKey] = value;
 
-        public int GetCurrentAttribute(AttributeKey attKey) => m_baseAttributes.TryGetValue(attKey, out var value)
-            ? value
-            : throw new ArgumentException($"Requested attribute [{attKey}] was never set.", nameof(attKey));
-
-        public void AddLayeredEffect(LayeredEffectDefinition effect)
+        public int GetCurrentAttribute(AttributeKey attKey)
         {
-            throw new NotImplementedException();
+            var baseAttribute = m_baseAttributes.TryGetValue(attKey, out var value)
+                ? value
+                : throw new ArgumentException($"Requested attribute [{attKey}] was never set.", nameof(attKey));
+
+            return m_effectDefinitions
+                .Where(definition => definition.Attribute == attKey)
+                .Select(definition => definition.Modification)
+                .Aggregate(baseAttribute, (attribute, modification) => attribute + modification);
         }
+
+        public void AddLayeredEffect(LayeredEffectDefinition effect) => m_effectDefinitions.Add(effect);
 
         public void ClearLayeredEffects()
         {
