be61088e5bb748523e8e1e17cea482836b6c5dd6
Jim Martin <jim.martin@unity3d.com>
2022-05-16 11:12:42 -0500

    Pass GetCurrentAttribute_WithBaseAttribute_ReturnsBaseAttribute

--- LayeredAttributes/SampleLayeredAttributes.cs
+++ LayeredAttributes/SampleLayeredAttributes.cs
@@ -1,18 +1,17 @@
 using System;
+using System.Collections.Generic;
 
 namespace LayeredAttributes
 {
     public class SampleLayeredAttributes : ILayeredAttributes
     {
-        public void SetBaseAttribute(AttributeKey attKey, int value)
-        {
-            throw new NotImplementedException();
-        }
+        private readonly Dictionary<AttributeKey, int> m_baseAttributes = new();
 
-        public int GetCurrentAttribute(AttributeKey attKey)
-        {
-            throw new ArgumentException($"Requested attribute [{attKey}] was never set.", nameof(attKey));
-        }
+        public void SetBaseAttribute(AttributeKey attKey, int value) => m_baseAttributes[attKey] = value;
+
+        public int GetCurrentAttribute(AttributeKey attKey) => m_baseAttributes.TryGetValue(attKey, out var value)
+            ? value
+            : throw new ArgumentException($"Requested attribute [{attKey}] was never set.", nameof(attKey));
 
         public void AddLayeredEffect(LayeredEffectDefinition effect)
         {
